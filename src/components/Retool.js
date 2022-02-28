6a2852c4327d8f6f3447454217d5b11e7202dc9b
Mitch Messecar <mitch@rocksolidrestaurants.com>
2022-02-28 09:25:02 -0800

    new2

---
+++ src/components/Retool.js
@@ -0,0 +1,91 @@
+import { useEffect, useRef, useState } from "react";
+
+const Retool = ({ data, url, height, width }) => {
+  const embeddedIframe = useRef(null);
+  const [elementWatchers, setElementWatchers] = useState({});
+
+  /* Retool passes up the list of elements to watch on page load  */
+  useEffect(() => {
+    for (const key in elementWatchers) {
+      const watcher = elementWatchers[key];
+      watcher.iframe?.contentWindow.postMessage(
+        {
+          type: "PARENT_WINDOW_RESULT",
+          result: data[watcher.selector],
+          id: watcher.queryId,
+          pageName: watcher.pageName,
+        },
+        "*"
+      );
+    }
+  }, [data, elementWatchers]);
+
+  /* On page load, add event listener to listen for events from Retool */
+  useEffect(() => {
+    /* Handle events - if PWQ then create/replace watchers -> return result */
+    const handler = (event) => {
+      if (!embeddedIframe?.current?.contentWindow) return;
+      if (event.data.type === "PARENT_WINDOW_QUERY") {
+        createOrReplaceWatcher(
+          event.data.selector,
+          event.data.pageName,
+          event.data.id
+        );
+        postMessageForSelector("PARENT_WINDOW_RESULT", event.data);
+      }
+    };
+
+    window.addEventListener("message", handler);
+
+    return () => window.removeEventListener("message", handler);
+  }, []);
+
+  /* Creates or updates the list of values for us to watch for changes */
+  const createOrReplaceWatcher = (selector, pageName, queryId) => {
+    const watcherId = pageName + "-" + queryId;
+    const updatedState = elementWatchers;
+
+    updatedState[watcherId] = {
+      iframe: embeddedIframe.current,
+      selector: selector,
+      pageName: pageName,
+      queryId: queryId,
+    };
+
+    setElementWatchers(updatedState);
+  };
+
+  /* Checks for selectors for data and posts message for Retool to read */
+  const postMessageForSelector = (messageType, eventData) => {
+    const maybeData = data[eventData.selector];
+
+    if (maybeData) {
+      embeddedIframe.current.contentWindow.postMessage(
+        {
+          type: messageType,
+          result: maybeData,
+          id: eventData.id,
+          pageName: eventData.pageName,
+        },
+        "*"
+      );
+    } else {
+      console.log(
+        `Not sending data back to Retool, nothing found for selector: ${eventData.selector}`
+      );
+    }
+  };
+  
+  return (
+    <iframe
+      height={height ?? "100%"}
+      width={width ?? "100%"}
+      frameBorder="none"
+      src={url}
+      ref={embeddedIframe}
+      title="retool"
+    ></iframe>
+  );
+};
+
+export default Retool;
